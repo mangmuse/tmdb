142e8b3e25643a12f0522ad53935c8241478b4b9
mangmuse <mangmangmuse@gmail.com>
2024-04-26 17:56:56 +0900

    refactor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 const bearerToken =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTQ0MTQ4ZWIzOTExZjlkNGY2MzAxZGY5YjBjOTZlMiIsInN1YiI6IjY2MjViMGM4NjJmMzM1MDE3ZGRhMDFhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.my0shwxxFlTtv5hBOjqmYeciNLHwcg3xR8WykTmWv5M";
 
@@ -16,8 +17,8 @@ async function getMovies() {
     const response = await fetch(url, options);
     const data = await response.json();
     return data.results;
-  } catch (error) {
-    console.error("Error fetching data: ", error);
+  } catch (e) {
+    console.error(e);
   }
 }
 
@@ -25,37 +26,40 @@ let allMovies = [];
 
 async function displayMovies(movies = allMovies, inputValue = "") {
   try {
-    const movieContainer = document.querySelector(".movie__container");
-    movieContainer.innerHTML = "";
     const filteredMovies = inputValue
       ? movies.filter((movie) =>
           movie.title.toLowerCase().includes(inputValue.toLowerCase())
         )
       : movies;
-    for (const movie of filteredMovies) {
-      const movieCard = document.createElement("li");
-      movieCard.className = "movie__card";
-      movieCard.setAttribute("data-key", movie.id);
-      movieCard.innerHTML = `
-                <div class="movie__img__container">
-                <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="${movie.title}" />
-                </div>
-                <div class="movie__detail">
-                <h2 class="movie__title">${movie.title}</h2>
-                <p class="movie__overview"">${movie.overview}</p>
-                <p class="movie__rating">Rating: ${movie.vote_average}</p>
-                </div>
-                    `;
-      //   img , title , overview, raiting
-      movieContainer.appendChild(movieCard);
-      movieCard.addEventListener("click", () => {
-        const movieId = movieCard.dataset.key;
-        alert(`id: ${movieId}`);
-      });
-    }
+    renderMovieCards(filteredMovies);
   } catch (e) {
     console.error(e);
     document.querySelector(".error__message").classList.remove("hidden");
+  }
+}
+
+function renderMovieCards(filteredMovies) {
+  const movieContainer = document.querySelector(".movie__container");
+  movieContainer.innerHTML = "";
+  for (const movie of filteredMovies) {
+    const movieCard = document.createElement("li");
+    movieCard.className = "movie__card";
+    movieCard.setAttribute("data-key", movie.id);
+    movieCard.innerHTML = `
+              <div class="movie__img__container">
+              <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="${movie.title}" />
+              </div>
+              <div class="movie__detail">
+              <h2 class="movie__title">${movie.title}</h2>
+              <p class="movie__overview"">${movie.overview}</p>
+              <p class="movie__rating">Rating: ${movie.vote_average}</p>
+              </div>
+                  `;
+    movieContainer.appendChild(movieCard);
+    movieCard.addEventListener("click", () => {
+      const movieId = movieCard.dataset.key;
+      alert(`id: ${movieId}`);
+    });
   }
 }
 
@@ -85,24 +89,24 @@ function sortMovies(movies, option) {
 }
 
 let inputValue = "";
-
 const headerForm = document.querySelector(".header__form");
 
-headerForm.addEventListener("submit", (e) => {
+const handleFormSubmit = (e) => {
   const searchInput = document.querySelector(".header__form--input");
   e.preventDefault();
   inputValue = searchInput.value;
   displayMovies(allMovies, inputValue);
   window.scrollTo(0, 0);
-});
-
-const sortOption = document.querySelector("#sort__option");
-
-sortOption.addEventListener("change", (e) => {
+};
+const handleSortChange = (e) => {
   const selectedOption = e.target.value;
   const sortedMovies = sortMovies(allMovies, selectedOption);
   displayMovies(sortedMovies, inputValue);
   window.scrollTo(0, 0);
-});
+};
+
+const sortOption = document.querySelector("#sort__option");
+headerForm.addEventListener("submit", handleFormSubmit);
+sortOption.addEventListener("change", handleSortChange);
 
 initMovies();
